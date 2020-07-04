b7dcfeddea6c1adbba403075b57453cbcee8cac9
figean <hengdu200485@126.com>
2020-07-04 16:10:17 +0800

    使用playground 可以很轻松的学习swift 语法

--- HelloUI3/goodBye.playground/section-1.swift
+++ HelloUI3/goodBye.playground/section-1.swift
@@ -14,3 +14,34 @@ println(possiableString2)
 
 NSLog("is there any problem")
 
+let b = 10
+var a = 10
+a != b
+
+"hello," + "cookie"
+
+9%2.5
+
+for index in 1...5
+{
+    println(index)
+}
+
+let dog = "dog!??"
+
+for codeUnit in dog.utf8
+{
+    println(codeUnit)
+}
+
+let ch : Character = "e"
+//使用switch 语法
+switch ch{
+    case "1","b","c","g","h","i":
+    println("okay")
+    
+    case "r","b","c","f","e","p":
+    println("it is okay")
+    default:
+    println("default")
+}
